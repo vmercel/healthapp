5c1f47e2de70c62fb160bdcf954462ceb6dce052
Vubangsi Mercel <vmercel@gmail.com>
2022-12-06 08:12:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,126 +1,98 @@
-(() => {
-    // The width and height of the captured photo. We will set the
-    // width to the value defined here, but the height will be
-    // calculated based on the aspect ratio of the input stream.
-  
-    const width = 320; // We will scale the photo width to this
-    let height = 0; // This will be computed based on the input stream
-  
-    // |streaming| indicates whether or not we're currently streaming
-    // video from the camera. Obviously, we start at false.
-  
-    let streaming = false;
-  
-    // The various HTML elements we need to configure or control. These
-    // will be set by the startup() function.
-  
-    let video = null;
-    let canvas = null;
-    let photo = null;
-    let startbutton = null;
-  
-    function showViewLiveResultButton() {
-      if (window.self !== window.top) {
-        // Ensure that if our document is in a frame, we get the user
-        // to first open it in its own tab or window. Otherwise, it
-        // won't be able to request permission for camera access.
-        document.querySelector(".contentarea").remove();
-        const button = document.createElement("button");
-        button.textContent = "View live result of the example code above";
-        document.body.append(button);
-        button.addEventListener("click", () => window.open(location.href));
-        return true;
+feather.replace();
+
+const controls = document.querySelector('.controls');
+const cameraOptions = document.querySelector('.video-options>select');
+const video = document.querySelector('video');
+const canvas = document.querySelector('canvas');
+const screenshotImage = document.querySelector('img');
+const buttons = [...controls.querySelectorAll('button')];
+let streamStarted = false;
+
+const [play, pause, screenshot] = buttons;
+
+const constraints = {
+  video: {
+    width: {
+      min: 1280,
+      ideal: 1920,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1080,
+      max: 1440
+    },
+  }
+};
+
+cameraOptions.onchange = () => {
+  const updatedConstraints = {
+    ...constraints,
+    deviceId: {
+      exact: cameraOptions.value
+    }
+  };
+
+  startStream(updatedConstraints);
+};
+
+play.onclick = () => {
+  if (streamStarted) {
+    video.play();
+    play.classList.add('d-none');
+    pause.classList.remove('d-none');
+    return;
+  }
+  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+    const updatedConstraints = {
+      ...constraints,
+      deviceId: {
+        exact: cameraOptions.value
       }
-      return false;
-    }
-  
-    function startup() {
-      if (showViewLiveResultButton()) {
-        return;
-      }
-      video = document.getElementById("video");
-      canvas = document.getElementById("canvas");
-      photo = document.getElementById("photo");
-      startbutton = document.getElementById("startbutton");
-  
-      navigator.mediaDevices
-        .getUserMedia({ video: true, audio: false })
-        .then((stream) => {
-          video.srcObject = stream;
-          video.play();
-        })
-        .catch((err) => {
-          console.error(`An error occurred: ${err}`);
-        });
-  
-      video.addEventListener(
-        "canplay",
-        (ev) => {
-          if (!streaming) {
-            height = video.videoHeight / (video.videoWidth / width);
-  
-            // Firefox currently has a bug where the height can't be read from
-            // the video, so we will make assumptions if this happens.
-  
-            if (isNaN(height)) {
-              height = width / (4 / 3);
-            }
-  
-            video.setAttribute("width", width);
-            video.setAttribute("height", height);
-            canvas.setAttribute("width", width);
-            canvas.setAttribute("height", height);
-            streaming = true;
-          }
-        },
-        false
-      );
-  
-      startbutton.addEventListener(
-        "click",
-        (ev) => {
-          takepicture();
-          ev.preventDefault();
-        },
-        false
-      );
-  
-      clearphoto();
-    }
-  
-    // Fill the photo with an indication that none has been
-    // captured.
-  
-    function clearphoto() {
-      const context = canvas.getContext("2d");
-      context.fillStyle = "#AAA";
-      context.fillRect(0, 0, canvas.width, canvas.height);
-  
-      const data = canvas.toDataURL("image/png");
-      photo.setAttribute("src", data);
-    }
-  
-    // Capture a photo by fetching the current contents of the video
-    // and drawing it into a canvas, then converting that to a PNG
-    // format data URL. By drawing it on an offscreen canvas and then
-    // drawing that to the screen, we can change its size and/or apply
-    // other changes before drawing it.
-  
-    function takepicture() {
-      const context = canvas.getContext("2d");
-      if (width && height) {
-        canvas.width = width;
-        canvas.height = height;
-        context.drawImage(video, 0, 0, width, height);
-  
-        const data = canvas.toDataURL("image/png");
-        photo.setAttribute("src", data);
-      } else {
-        clearphoto();
-      }
-    }
-  
-    // Set up our event listener to run the startup process
-    // once loading is complete.
-    window.addEventListener("load", startup, false);
-  })();
+    };
+    startStream(updatedConstraints);
+  }
+};
+
+const pauseStream = () => {
+  video.pause();
+  play.classList.remove('d-none');
+  pause.classList.add('d-none');
+};
+
+const doScreenshot = () => {
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+  canvas.getContext('2d').drawImage(video, 0, 0);
+  screenshotImage.src = canvas.toDataURL('image/webp');
+  screenshotImage.classList.remove('d-none');
+};
+
+pause.onclick = pauseStream;
+screenshot.onclick = doScreenshot;
+
+const startStream = async (constraints) => {
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+  handleStream(stream);
+};
+
+
+const handleStream = (stream) => {
+  video.srcObject = stream;
+  play.classList.add('d-none');
+  pause.classList.remove('d-none');
+  screenshot.classList.remove('d-none');
+
+};
+
+
+const getCameraSelection = async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const videoDevices = devices.filter(device => device.kind === 'videoinput');
+  const options = videoDevices.map(videoDevice => {
+    return `<option value="${videoDevice.deviceId}">${videoDevice.label}</option>`;
+  });
+  cameraOptions.innerHTML = options.join('');
+};
+
+getCameraSelection();
